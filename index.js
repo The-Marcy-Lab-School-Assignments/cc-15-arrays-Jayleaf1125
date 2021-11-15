c31cf24563c70eeb48357a07af240da85f8cb025
Jayleaf1125 <Jasonpadilla104@gmail.com>
2021-11-15 15:08:57 +0000

    Code Challenge 15 completed

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 //write your code here
+const findLongestWord = words => {
+    let longestWord = "";
+    for(let word of words) {
+        if(word.length > longestWord.length) {
+             longestWord = word;
+        } else {
+            continue
+        }
+    }
+     return longestWord;
+}
+
+console.log(findLongestWord(["jazzy", "jumpy", "quaky"])) //returns "jazzy"
